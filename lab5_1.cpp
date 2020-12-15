29524c7d47058f28a46ee0124f8bfdcb573e259a
Som3on31 <76034350+Som3on31@users.noreply.github.com>
2020-12-15 16:14:14 +0700

    lap5_1.cpp completed

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,9 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
+
+    int a=1,i=0,b=0,c=0;
+    while (!(a==0)){
+
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> a;
+
+    if (a%2==0){
+        b=b+1;
+    }else{
+        c=c+1;
+        }
+    }
+    b--;
+    cout << "#Even numbers = " << b << "\n";
+    cout << "#Odd numbers = " << c;
 }
